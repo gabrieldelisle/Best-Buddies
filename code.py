9f32db0d98afeee29fa8c87aad0b4d6aa9645461
Gabriel de Lisle <gldl@kth.se>
2019-05-02 15:16:33 +0200

    loop

--- code.py
+++ code.py
@@ -41,7 +41,7 @@ def neighbours(x, size, n) :
 		if x+i >=0 and x+i <n
 	]
 
-def correlation(CA, CB) :
+def correlation(CA, CB, size) :
 	n = CA.shape[2]
 
 	corr = torch.zeros((n*n,n*n))
@@ -49,23 +49,22 @@ def correlation(CA, CB) :
 		for py in range(n):
 			for qx in range(n):
 				for qy in range(n):
-					ca = CA[:,:,neighbours(px, 3, n),:][:,:,:,neighbours(py, 3, n)]
-					cb = CB[:,:,neighbours(qx, 3, n),:][:,:,:,neighbours(qy, 3, n)]
+					ca = CA[:,:,neighbours(px, size, n),:][:,:,:,neighbours(py, size, n)]
+					cb = CB[:,:,neighbours(qx, size, n),:][:,:,:,neighbours(qy, size, n)]
 					dot = torch.einsum("ijkl,ijuv->kluv", (ca,cb))
 					normA = torch.sqrt(torch.einsum("ijkl,ijkl->kl", (ca,ca)))
 					normB = torch.sqrt(torch.einsum("ijkl,ijkl->kl", (cb,cb)))
 					normA = normA.view(*normA.shape, 1, 1)
 					normB = normB.view(1, 1, *normB.shape)
-					corr[px * n + py, qx * n + qy] = torch.sum(dot / normA / normB)
+					corr[px * n + py, qx * n + qy] = torch.sum (dot / normA / normB)
 	return corr
 
 
 def bestBuddies(FA, FB):
 	n = FA.shape[2]
-	
-	CA, CB = normalize(FA, FB)
-	corr = correlation(CA, CB)
 
+	CA, CB = normalize(FA, FB)
+	corr = correlation(CA, CB, 3)
 	# filter for keeping only significative activations
 	normA = torch.sqrt(torch.einsum("ijkl,ijkl->kl", (FA,FA)))
 	normB = torch.sqrt(torch.einsum("ijkl,ijkl->kl", (FB,FB)))
@@ -80,7 +79,6 @@ def bestBuddies(FA, FB):
 	for p in range(corr.shape[0]):
 		q = torch.argmax(corr[p,:]).item()
 		if torch.argmax(corr[:,q]).item() == p:
-			print(p,q)
 			px, py = p//n, p%n
 			qx, qy = q//n, q%n
 			if HA[px, py] > gamma and HB[qx, qy] > gamma: 
@@ -89,17 +87,46 @@ def bestBuddies(FA, FB):
 	return bbA, bbB
 
 
+radius_list=[4,4,6,6] #use radius_list[l-2] for l=5to2
+l_layer=[5,10,19,28,37] #use l_layer[l-1] for l=5to1
+
+def next_layer(R,l):
+	for regions in R:
+		print(regions[0].shape)
+		new_R=[]
+		bbA, bbB = bestBuddies(*regions)
+		print(bbA, bbB)
+		for k in range(len(bbA)):
+			new_R.append([])
+			px, py = bbA[k]
+			qx, qy = bbB[k]
+			new_R[k].append(FA[l_layer[l-1]][:,:,neighbours(2*px, radius_list[l-2]+1, FA[l_layer[l-1]].shape[2]),:][:,:,:,neighbours(2*py, radius_list[l-2]+1, FA[l_layer[l-1]].shape[2])])
+			new_R[k].append(FB[l_layer[l-1]][:,:,neighbours(2*qx, radius_list[l-2]+1, FB[l_layer[l-1]].shape[2]),:][:,:,:,neighbours(2*qy, radius_list[l-2]+1, FB[l_layer[l-1]].shape[2])])
+	return new_R
+
+
+
 VGG19 = models.vgg19(pretrained=True)
 
 imA = load("original_A.png")
 FA = forward_pass(imA, VGG19)
 
-imB = load("original_A.png")
+imB = load("original_B.png")
 FB = forward_pass(imB, VGG19)
 
 
+R = [[FA[-1], FB[-1]]]
+for l in range(4,0,-1) :
+	print(l)
+	R = next_layer(R,l)
 
-print(bestBuddies(FA[-1], FB[-1]))
+
+
+
+
+
+
+# print(bestBuddies(FA[-1], FB[-1]))
 
 
 
